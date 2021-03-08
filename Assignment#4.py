b8ea2d4d0616245246471d3627e99f5c42af6b0f
AHA91 <77797151+AHA91@users.noreply.github.com>
2021-03-07 22:29:56 -0500

    Update and rename Assignment4.py to Assignment#4.py

--- Assignment#4.py
+++ Assignment#4.py
@@ -1,52 +1,53 @@
-import numpy as np
+###Alex Andronikides
+###Phys 661 
 import sympy as sp
 
 ################# Part 1################
 
 def FPI(f,x0,Itter):
     itr = 0
-
     while itr < Itter:
         x = f(x0)
         print('Iteration %d: x = %0.3f, f(x) = %0.3f' % (itr, x, f(x)))
         x0 = x
         itr = itr + 1
-    print('\n Required root is: %0.3f' % x)
+        if f(x) == 0.000:
+            print('\n Required root is: %0.3f' % x)
+            break
     
 
 ################ Part 2 ################
-# Printing Iteration issue
+
 
 def NRM(f,x0,Itter):
     x = sp.Symbol('x')
     f = eval(f)
     df = sp.diff(f,x) 
     itr = 0
-
     while itr < Itter:
         x_ = x0 - (f.subs(x,x0).evalf())/(df.subs(x,x0).evalf())
-        print('Iteration %d: x_ = %0.3f, f(x_) = %0.3f' % (itr, x_, f(x_)))
-        x0 = x_ 
+        print('Iteration '+ str(itr) + ": "+'%.3f %.3f' % (x_, f.subs(x_,x)))
+        x0 = x_
         itr = itr + 1
-    print('\n Required root is: %0.3f' % x_)
+        if f.subs(x_,x) == 0.000:
+            print('\n Required root is: %0.3f' % x_)
+            break
 
 
 ############### Part 3 ##################
 
 def SCM(f,x0,x1,Itter):
     itr = 0
-
     while itr < Itter:
-        if f(x0) == f(x1):
-            print("Error: Divide by zero")
-            break               
-        x2 = x0 - f(x0)*(x1-x0)/(f(x1)-f(x0))
-        print('Iteration %d: x2 = %0.3f, f(x2) = %0.3f' % (itr, x2, f(x2)))
+        x2 = x1 - f(x1)*((x1-x0)/(f(x1)-f(x0)))
         x0 = x1
-        x1 = x2                        
+        x1 = x2
+        print('Iteration %d: x2 = %0.3f, f(x2) = %0.3f' % (itr, x2, f(x2)))
         itr = itr + 1
-    print('\n Required root is: %0.3f' % x2)
-        
+        if f(x2) == 0.000:
+            print('\n Required root is: %0.3f' % x2)
+            break
+         
        
     
     
