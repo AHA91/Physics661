e54bbb6026389bc006a1c2080d393b2f3a8aeb97
AHA91 <77797151+AHA91@users.noreply.github.com>
2021-03-08 09:27:29 -0500

    Update Assignment#4.py

--- Assignment#4.py
+++ Assignment#4.py
@@ -9,11 +9,12 @@ def FPI(f,x0,Itter):
     while itr < Itter:
         x = f(x0)
         print('Iteration %d: x = %0.3f, f(x) = %0.3f' % (itr, x, f(x)))
-        x0 = x
-        itr = itr + 1
         if f(x) == 0.000:
             print('\n Required root is: %0.3f' % x)
             break
+        x0 = x
+        itr = itr + 1
+
     
 
 ################ Part 2 ################
@@ -26,12 +27,12 @@ def NRM(f,x0,Itter):
     itr = 0
     while itr < Itter:
         x_ = x0 - (f.subs(x,x0).evalf())/(df.subs(x,x0).evalf())
-        print('Iteration %d: x = %0.3f, f(x) = %0.3f' % (itr, x_, f.subs(x, x_)))
         x0 = x_
-        itr = itr + 1
-        if f.subs(x_,x) == 0.000:
+        print('Iteration %d: x = %0.3f, f(x) = %0.3f' % (itr, x_, f.subs(x, x_)))
+        if f.subs(x,x_) == 0.000:
             print('\n Required root is: %0.3f' % x_)
             break
+        itr = itr + 1
 
 
 ############### Part 3 ##################
@@ -39,15 +40,17 @@ def NRM(f,x0,Itter):
 def SCM(f,x0,x1,Itter):
     itr = 0
     while itr < Itter:
+        if f(x0) == f(x1):
+            print("Error: Divide by zero")
+            break 
         x2 = x1 - f(x1)*((x1-x0)/(f(x1)-f(x0)))
         print('Iteration %d: x2 = %0.3f, f(x2) = %0.3f' % (itr, x2, f(x2)))
-        x0 = x1
-        x1 = x2
-        itr = itr + 1
         if f(x2) == 0.000:
             print('\n Required root is: %0.3f' % x2)
             break
-         
-       
+        x0 = x1
+        x1 = x2
+        itr = itr + 1
+
     
     
